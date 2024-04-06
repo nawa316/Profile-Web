036ef3e489401c8df73c24441248fe6ba56fd27e
Muhammad Ade Dzakwan <muhammadadedzakwan@gmail.com>
2024-04-06 13:06:04 +0700

    Create test.php

---
+++ assets/php/test.php
@@ -0,0 +1,17 @@
+<?php
+$nama = "Muhammad Ade Dzakwan";
+$nomorHp = 6289513601357;
+$operasi = 5;
+$sedangBelajar = true;
+$namaBahasaPemrograman = ["Javascript, Java, PHP"];
+
+$penjumlahan = $operasi + 1;
+$pengurangan = $operasi - 3;
+$perkalian = $operasi * 2;
+$pembagian = $operasi / 2;
+
+echo $penjumlahan;
+echo $pengurangan;
+echo $perkalian;
+echo $pembagian;
+?>
