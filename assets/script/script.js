b3492512a76f5d203a0559759dc5e05cfecb7434
nawa316 <muhammadadedzakwan@gmail.com>
2024-09-15 08:17:52 +0700

    feat: adding button functionality

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+function showNav() {
+  if (document.getElementById("navigator").style.display == "none") {
+    document.getElementById("navigator").style.display = "flex";
+    document.getElementById("button-menu").style.backgroundColor = "#808080";
+  } else {
+    document.getElementById("navigator").style.display = "none";
+    document.getElementById("button-menu").style.backgroundColor = "white";
+  }
+}
