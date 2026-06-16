2c84121392c1fb96ab5d6bb85a98c9a2b7edf73b
nawa316 <muhammadadedzakwan@gmail.com>
2026-06-16 19:32:35 +0700

    fix: error deploy

--- src/components/pdf/PDFTemplate.tsx
+++ src/components/pdf/PDFTemplate.tsx
@@ -185,6 +185,7 @@ export const PDFTemplate = forwardRef<HTMLDivElement, PDFTemplateProps>(
                   )}
                 </div>
               </div>
+            </div>
             ))}
           </div>
         </div>
